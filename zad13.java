a45ffdd3bf7e3cbc6884a27038fe342b74c029ea
Andrzej <andrzej.milewczyk1@gmail.com>
2019-11-21 16:03:14 +0100

    test java

--- zad13.java
+++ zad13.java
@@ -1,15 +1,27 @@
 package java_98_388;
 
 public class zad13 {
-    public static void traverse(String board) {
-        for (int x = 0; x < board.length(); x++) {
-            for (int y = 0; y < board.length(); y++) {
+    String board;
+    public static void traverse(String[][] board) {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board.length; y++) {
                 System.out.println(board);
             }
         }
     }
     public static void main(String[] args) {
 
-        traverse("w2222222");
+
+        String[][] board2 = new String[1][2];
+        for (int i = 0; i < board2.length ; i++) {
+            for (int j = 0; j < board2.length ; j++) {
+                System.out.println(board2);
+
+            }
+
+        }
+
+
+
     }
 }
---
+++ zad14.java
@@ -0,0 +1,15 @@
+package java_98_388;
+
+public class zad14 {
+    public static void main(String[] args) {
+
+        char[][] grid = {
+                {'-', '-', 'X'},
+                {'-', '-', 'X'},
+                {'-', 'O', '-'}
+        };
+        System.out.println(grid[0][2]);
+        System.out.println(grid[2][1]);
+
+    }
+}
